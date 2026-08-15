42ae495ab209a1a4fb27ffdc6745124d19159bc6
vatsautsav <vatsautsav7@gmail.com>
2026-08-15 13:09:42 +0530

    Add files via upload

---
+++ address.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    printf("AT + PO - Bihat \n");
+    printf("Tola - Gurudaspur \n");
+    
+    printf("Ward No. 26 \n");
+    printf("Near Pir Asthan \n");
+    printf("Bihat \n");
+    printf("Begusarai \n");
+    return 0;
+
+}
